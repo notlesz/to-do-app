4ae6de6f184e1e4965324787a953193929b2a499
Elton Souza <elton.souza1515@gmail.com>
2022-11-15 14:06:02 -0300

    update: upadate prisma schema and new migration

---
+++ server/prisma/migrations/20221115153038_create_db_structure/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "StatusType" AS ENUM ('A_fazer', 'Em_execucao', 'Feito');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "profilePicture" TEXT,
+    "email" TEXT,
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "runtime" INTEGER,
+    "status" "StatusType" NOT NULL DEFAULT 'A_fazer',
+    "ownerId" INTEGER,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
