a662921e542054b253499a66f15bd18bea21e6cb
Elton Souza <elton.souza1515@gmail.com>
2022-11-29 18:35:38 -0300

    feat: update private key for env

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 import jsonwebtoken from "jsonwebtoken";
+
+dotenv.config();
 
 const PRIVATE_KEY = process.env.SECRET_KEY || "development";
 
