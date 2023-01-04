9610dc3b3acac49d61f90605cac07525a78b0f25
Elton Souza <elton.souza1515@gmail.com>
2023-01-04 08:44:55 -0300

    server-fix: public and private routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import { validateToken } from "../middleware/auth";
-import userPrivateRoute from "./userPrivateRoute";
+import privateRoute from "./privateRoute";
 import publicRoute from "./publicRoute";
-import taskPrivateRoute from "./taskPrivateRoute";
 
 const routes = express();
 
-routes.use(publicRoute);
-routes.use('*', validateToken);
-routes.use(taskPrivateRoute)
-routes.use(userPrivateRoute);
+routes.use("/", publicRoute);
+routes.use("*", validateToken);
+routes.use("/user", privateRoute);
 
 export default routes;
--- server/src/routes/publicRoute.ts
+++ server/src/routes/publicRoute.ts
@@ -1,17 +1,15 @@
-import { Prisma } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { Router } from "express";
 import jsonwebtoken from "jsonwebtoken";
 import UserController from "../controllers/userController";
 import { PRIVATE_KEY } from "../middleware/auth";
 import { prisma } from "../prisma";
-import { validatePassword } from "../util/validates";
 
 const userController = new UserController();
 
 const publicRoute = Router();
 
-publicRoute.post("/login", async (req, res) => {
+publicRoute.post("/singin", async (req, res) => {
   const [, hash] = req.headers.authorization?.split(" ") || [" ", " "];
   const [email, password] = Buffer.from(hash, "base64")?.toString()?.split(":");
 
@@ -21,69 +19,27 @@ publicRoute.post("/login", async (req, res) => {
     },
   });
 
-  if (!user) return res.status(401).send("E-mail ou senha incorreta!");
+  if (!user) return res.status(400).send("Usuário não registrado!");
 
-  const verifyPassword = bcrypt.compareSync(password, user?.password!);
+  const verifyPassword = bcrypt.compareSync(password, user.password!);
 
-  if (!verifyPassword)
-    return res.status(401).send("E-mail ou senha incorreta!");
+  if (!verifyPassword) return res.status(400).send("Senha incorreta!");
 
-  const userToken = jsonwebtoken.sign(
+  const token = jsonwebtoken.sign(
     {
       user: JSON.stringify({
-        name: user?.name,
-        email: user?.email,
-        id: user?.id,
+        name: user.name,
+        email: user.email,
+        id: user.id,
       }),
     },
     PRIVATE_KEY,
-    { expiresIn: "6h" }
+    { expiresIn: "12h" }
   );
 
-  res.send(userToken);
+  res.status(201).send(token);
 });
 
-publicRoute.post("/user/register", async (req, res) => {
-  const { name, email, password, profilePicture } = req.body;
-
-  try {
-    const validatedPassword = validatePassword(password);
-
-    if (!validatedPassword.isValid)
-      return res.status(401).send(validatedPassword.message);
-
-    const encryptedPassword = bcrypt.hashSync(password, 8);
-
-    const userCreated = await userController.createUser({
-      name,
-      email,
-      password: encryptedPassword,
-      profilePicture,
-    });
-
-    const userToken = jsonwebtoken.sign(
-      {
-        user: JSON.stringify({
-          name: userCreated?.name,
-          email: userCreated?.email,
-          id: userCreated?.id,
-        }),
-      },
-      PRIVATE_KEY,
-      { expiresIn: "24h" }
-    );
-
-    res.status(201).send({
-      token: userToken,
-      user: userCreated,
-    });
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      if (error.code === "P2002") {
-        res.status(404).send("Email já existente.");
-      }
-    }
-  }
-});
+publicRoute.post("/signup", userController.registerUser);
 
 export default publicRoute;
